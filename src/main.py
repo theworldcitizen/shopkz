dc8db6e94c779aca7b5e2f63f6e265ce0411bd25
Zhanerke Tattibek <zhvneka@gmail.com>
2022-09-10 19:40:19 +0600

    parser for the site shop.kz

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-import json
 from typing import List
+
 from fastapi import FastAPI
 import uvicorn
-from crawler.crawler import Crawler
+from crawler.readfile import Readfile
+from models import Ad
 
 app = FastAPI()
 
@@ -12,5 +13,26 @@ async def main():
     return {'status': 'ok'}
 
 
-# localhost:8000/smartphones?price=329900
-@app.get('/smartphones/{price}', response_model=)
+@app.get('/smartphones/{price}', response_model=List[Ad])
+async def get_data(price):
+    obj = Readfile()
+    result = obj.read_file()
+    answer = []
+
+    for x in result:
+        try:
+            a = x.get('price').split()
+            new_price = ''
+            del a[-1]
+            for el in a:
+                new_price += el
+            if new_price == price:
+                answer.append(x)
+        except Exception:
+            continue
+
+    return answer
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
